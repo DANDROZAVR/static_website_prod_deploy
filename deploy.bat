3fac923b413c007c69f2885ff2fa9ced8fe6c169
Ignas <13539780+ignasposka@users.noreply.github.com>
2018-05-28 20:15:58 +0300

    Changed naming to make script more readable.

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo OFF
-powershell -Command "plink -ssh -i D:\your\path\to\private\key\id_rsa.ppk -noagent root@11.111.111.11 /root/scripts/make_website_backup.sh yourwebsitename"
+powershell -Command "plink -ssh -i D:\your\path\to\private\key\id_rsa.ppk -noagent root@11.111.111.11 /root/scripts/yourScriptInServer.sh yourwebsitename"
 powershell -Command "WinSCP.exe /console /script='D:\\your\\path\\to\\winscp\\script\\prod_to_server.txt'"
